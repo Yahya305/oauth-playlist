2452f32cc261dac62a52fdd6684347464030cce9
Yahya305 <saimyahya47@gmail.com>
2024-03-06 21:47:00 +0500

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -10,6 +10,8 @@ passport.use(
         clientSecret: keys.google.clientSecret,
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
+        createUser(profile.id,profile.displayName)
+
         // check if user already exists in our own db
         User.findOne({googleId: profile.id}).then((currentUser) => {
             if(currentUser){
@@ -27,5 +29,24 @@ passport.use(
                 });
             }
         });
+    })
+);
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
+const User = require('../models/user-model');
+const createUser  = require('../services/userService');
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log('passport callback function fired:');
+        console.log(profile);
+        createUser(profile.id,profile.displayName)
     })
 );
